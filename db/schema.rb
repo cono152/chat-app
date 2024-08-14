e05e9742883f68c1aad4186cd55d8e3eab2cb43a
cono152 <o9o97444159@yahoo.co.jp>
2024-08-14 16:50:55 +0900

    roomsテーブルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_08_07_090906) do
+ActiveRecord::Schema[7.0].define(version: 2024_08_14_070654) do
+  create_table "rooms", charset: "utf8", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", charset: "utf8", force: :cascade do |t|
     t.string "name", null: false
     t.string "email", default: "", null: false
